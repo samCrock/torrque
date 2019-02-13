e2a7ff605b0559afc7aed067f5ae632a209b23f8
sam <samlombardi86@gmail.com>
2019-02-13 14:28:17 +0100

    Added dist

---
+++ dist/torrque/scripts.js
@@ -0,0 +1,14 @@
+chrome.webRequest.onBeforeSendHeaders.addListener(
+  function (details) {
+    console.log('details.requestHeaders', details.requestHeaders);
+    details.requestHeaders.push({
+      name: 'Origin',
+      value: 'http://localhost'
+    })
+    return {
+      requestHeaders: details.requestHeaders
+    };
+  }, { urls: ["<all_urls>"] }, ["blocking", "requestHeaders"]);
+
+;
+//# sourceMappingURL=scripts.js.map
